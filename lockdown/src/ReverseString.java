42c8aeb39fb19410184d734a62724154387286af
ajayr <ajayr@197nodnb9000033>
2020-04-14 17:14:43 +0530

    string reverse program

---
+++ lockdown/src/ReverseString.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String arg[]) {
+		
+		Scanner sc= new Scanner(System.in);
+		String input=sc.nextLine();
+		String rev="";
+		
+		for(int i=input.length()-1; i >=0; i --)
+		{
+			rev= rev+ input.charAt(i);
+		}
+				
+		System.out.println(rev);
+		
+		
+	}
+}
